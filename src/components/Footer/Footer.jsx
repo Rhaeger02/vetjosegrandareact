a156a2e2192857dc4f04aa00019b691c321f1e54
Rhaegar02 <balerion.tech.pe@outlook.com>
2026-01-09 00:24:56 -0500

    Footer improvement

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,9 @@ const Footer = () => {
   return (
     <footer>
       <p>
-        VETERINARIA JOSÉ GRANDA | EL CARIÑO QUE TU MEJOR AMIGO MERECE
-      </p>
+        VETERINARIA JOSÉ GRANDA | EL CARIÑO QUE TU MEJOR AMIGO MERECE | CREATED BY 
+        <a href="https://cma-portfolio.netlify.app/" target="_blank"> CARLOS M.</a>
+        </p>
     </footer>
   )
 }
